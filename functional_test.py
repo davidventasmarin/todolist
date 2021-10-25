730bc154d7378e3d2c7b4db6f12a28358d21f09f
davidventasmarin <dvm@medlinetec.eu>
2021-10-25 10:36:02 +0200

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -24,13 +25,23 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_test)
 
         #She is invited to enter a to-do item straight away
-        inputbox = self.navegador.find_elements_by_id('id_new_item')
-        self.assertEqual
+        inputbox = self.navegador.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 
+                        'Enter a to-do item')
         #She types "Buy peacock feathers" into a text box (Edith's hobby
         #is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         #When she hits enter, the page updates, and how the page lists
         #"1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.navegador.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.test == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
+        )
 
         #There is still a text box inviting her to add another item. She
         #enters "User peacock feathers to make a fly" (Edith is very methodical)
