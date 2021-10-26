a81b75e869d4600d868e87ca88e5be0320488986
davidventasmarin <dvm@medlinetec.eu>
2021-10-26 11:01:52 +0200

    Saving user input and testing database.

--- functional_test.py
+++ functional_test.py
@@ -31,22 +31,30 @@ class NewVisitorTest(unittest.TestCase):
         #She types "Buy peacock feathers" into a text box (Edith's hobby
         #is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
-
+        time.sleep(2)
         #When she hits enter, the page updates, and how the page lists
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         table = self.navegador.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.test == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         #There is still a text box inviting her to add another item. She
         #enters "User peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.navegador.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         #The page updates again, and now shows both items on her list
+        table = self.navegador.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         #Edith wonders whether the site will remember her list. Then she sees
         #that the site has generated a unique URL for her -- there is some
