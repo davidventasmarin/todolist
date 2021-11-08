50e8daa4db6c005174476a366fcd798922358bc8
davidventasmarin <dvm@medlinetec.eu>
2021-11-08 10:04:59 +0100

    Working incrementally, Ensuring We have a regression Test (Not complet)

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -67,10 +67,11 @@ class NewVisitorTest(LiveServerTestCase):
         #Edith wonders whether the site will remember her list. Then she sees
         #that the site has generated a unique URL for her -- there is some
         #explanatory text to that effect.
-
+        self.fail("finish the test!")
+        
         #She visits that URL - her to-do list is still there.
 
         #Satisfied, she goes back to sleep
 
-        self.fail("finish the test!")
+        
 
