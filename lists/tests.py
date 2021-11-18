2a52e8ca9b7462526db1922be5fbd8d078432662
davidventasmarin <dvm@medlinetec.eu>
2021-11-18 12:40:56 +0100

    7.9 finish and solved the error database

--- lists/tests.py
+++ lists/tests.py
@@ -3,29 +3,12 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second (ever) list item'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'The second (ever) list item')
-
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
@@ -35,16 +18,20 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
     
-    def test_display_all_list_item(self):
-        Item.objects.create(text='Itemey 1')
-        Item.objects.create(text='Itemey 2')
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+
+        Item.objects.create(text='Itemey 1', list=list_)
+        Item.objects.create(text='Itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
         
         self.assertContains(response, 'Itemey 1')
         self.assertContains(response, 'Itemey 2')
 
+
 class NewListTest(TestCase):
+
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
@@ -56,3 +43,32 @@ class NewListTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+class ListAnItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second (ever) list item'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'The second (ever) list item')
+        self.assertEqual(second_saved_item.list, list_)
